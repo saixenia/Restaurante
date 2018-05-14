9217810433c35cdd6286176b7bc2026bb331f27a
Julian Rodriguez <julian-rodriguez92@hotmail.com>
2018-05-14 16:33:10 -0500

    1

--- app/src/main/java/com/juanp/restaurante/LoginActivity.java
+++ app/src/main/java/com/juanp/restaurante/LoginActivity.java
@@ -40,7 +40,8 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    EditText Txt_Identificacion, ;
+    EditText Txt_Identificacion, Txt_Contrasena;
+    public Cursor Fila;
     TextView Txt_Registro;
 
     /**
@@ -84,10 +85,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
 
         // Set up the login form.
-        mEmailView = findViewById(R.id.email);
+        mEmailView = findViewById(R.id.Txt_Identificacion);
         populateAutoComplete();
 
-        mPasswordView = findViewById(R.id.password);
+        mPasswordView = findViewById(R.id.Txt_Contrasena);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -99,7 +100,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = findViewById(R.id.Btn_Iniciar_Sesion);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
